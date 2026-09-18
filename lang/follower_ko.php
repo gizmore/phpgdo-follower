2a9f769908adffeb7ea93822925140e97126e58d
gizmore <gizmore@wechall.net>
2026-09-18 04:07:51 +0200

    'more korean'

---
+++ lang/follower_ko.php
@@ -0,0 +1,16 @@
+<?php
+return [
+	'module_follower' => '추종자',
+	'btn_unfollow' => '언팔로우',
+	'link_follow' => '팔로우',
+	'link_followers' => '추종자',
+	'link_following' => '팔로잉',
+	'mt_follower_follow' => '[%s] 사용자 팔로우',
+	'err_follow_self' => '당신은 자신을 따라갈 수 없습니다.',
+	'err_follow_already' => '이미 %s을(를) 팔로우하고 있습니다.',
+	'msg_following' => '이제 %s을(를) 팔로우하고 있습니다.',
+	'list_followers' => '[%s] 추종자',
+	'list_following' => '[%s] 팔로잉',
+	'mt_follower_unfollow' => '[%s] 사용자 팔로우 중지',
+	'msg_unfollow' => '%s 팔로우를 중단했습니다.',
+];
